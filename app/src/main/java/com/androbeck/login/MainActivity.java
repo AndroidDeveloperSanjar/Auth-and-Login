f5c843ca9aadf50cd8a794e9063f4d2c1ee1a19e
AndroidDeveloperSanjar <45604294+AndroidDeveloperSanjar@users.noreply.github.com>
2020-09-24 18:46:21 +0500

    replace from activity to activity

--- app/src/main/java/com/androbeck/login/MainActivity.java
+++ app/src/main/java/com/androbeck/login/MainActivity.java
@@ -2,13 +2,36 @@ package com.androbeck.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import br.com.sapereaude.maskedEditText.MaskedEditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private LinearLayout nextBtn;
+    private MaskedEditText phoneNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViews();
+
+        nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,SetCodeActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public void findViews(){
+        nextBtn = findViewById(R.id.ll_next);
+        phoneNumber = findViewById(R.id.met_phone_number);
     }
 }
